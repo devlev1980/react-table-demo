404677c3bacddc96746284e7109771c4b0e2795c
yevgenylevin <devlev1980@gmail>
2020-11-29 11:50:24 +0200

    add project

---
+++ src/components/React-Table/Columns.js
@@ -0,0 +1,34 @@
+export const COLUMNS = [
+    {
+        Header: 'Id',
+        Footer: 'Id',
+        accessor: 'id'
+
+    },
+    {
+        Header: 'First name',
+        Footer: 'First name',
+        accessor: 'first_name'
+    },
+    {
+        Header: 'Last name',
+        Footer: 'Last name',
+        accessor: 'last_name'
+    },
+    {
+        Header: 'Email',
+        Footer: 'Email',
+        accessor: 'email'
+    },
+    {
+        Header: 'Birth date',
+        Footer: 'Birth date',
+        accessor: 'date_of_birth'
+    }
+]
+
+export const GROUPED_COLUMNS = [
+    {
+
+    }
+]
